42e3e187ab8369f32104db4f5d261724cb7d5ef3
DAIGO <dai31gose@gmail.com>
2025-08-23 16:11:27 +0900

    ハッカソン入門テストPython

---
+++ example.py
@@ -0,0 +1,26 @@
+# 1
+print("hello world")
+
+# 2
+def greet():
+    print("hello")
+
+greet()
+
+# 3
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print_name("DAIGO")
+
+# 4
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+# 5
+def add(a,b):
+    return a + b
+
+print(add(2,7))
